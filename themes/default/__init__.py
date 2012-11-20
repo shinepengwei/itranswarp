5ce168f4c6c52e8d5d800a6f50832c3a97159b73
Michael Liao <askxuefeng@gmail.com>
2012-11-20 21:15:55 +0800

    add default.__init__

---
+++ themes/default/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+name = 'Default'
+description = 'The default theme'
+author = 'Michael Liao'
+copyright = 'Copyright(c) 2012'
