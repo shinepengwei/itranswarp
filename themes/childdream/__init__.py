fa9a94644238e26006f43d58ddbe689e1207bf96
Michael Liao <askxuefeng@gmail.com>
2012-12-28 18:54:45 +0800

    update theme" childdream"

---
+++ themes/childdream/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+name = 'Child\'s Dream'
+description = 'The child\'s dream theme'
+author = 'Michael Liao'
+copyright = 'Copyright(c) 2012'
