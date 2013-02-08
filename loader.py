eecf3325781d2f2064065672748701a03ebb2266
Michael Liao <askxuefeng@gmail.com>
2013-02-08 15:03:16 +0800

    add module loader to load module at runtime

---
+++ loader.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+' Loader module that load modules dynamic. '
+
+import os, logging
+
+def load_module(module_name):
+    '''
+    Load a module and return the module reference.
+    '''
+    pos = module_name.rfind('.')
+    if pos==(-1):
+        return __import__(module_name, globals(), locals(), [module_name])
+    return __import__(module_name, globals(), locals(), [module_name[pos+1:]])
+
+def scan_submodules(module_name):
+    '''
+    Scan sub modules and import as dict (key=module name, value=module).
+
+    >>> ms = scan_submodules('apps')
+    >>> type(ms['article'])
+    <type 'module'>
+    >>> ms['article'].__name__
+    'apps.article'
+    >>> type(ms['manage'])
+    <type 'module'>
+    >>> ms['manage'].__name__
+    'apps.manage'
+    '''
+    web_root = os.path.dirname(os.path.abspath(__file__))
+    mod_path = os.path.join(web_root, *module_name.split('.'))
+    if not os.path.isdir(mod_path):
+        raise IOError('No such file or directory: %s' % mod_path)
+    dirs = os.listdir(mod_path)
+    mod_dict = {}
+    for name in dirs:
+        if name=='__init__.py':
+            continue
+        p = os.path.join(mod_path, name)
+        if os.path.isfile(p) and name.endswith('.py'):
+            pyname = name[:-3]
+            mod_dict[pyname] = __import__('%s.%s' % (module_name, pyname), globals(), locals(), [pyname])
+        if os.path.isdir(p) and os.path.isfile(os.path.join(mod_path, name, '__init__.py')):
+            mod_dict[name] = __import__('%s.%s' % (module_name, name), globals(), locals(), [name])
+    return mod_dict
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
+ 
