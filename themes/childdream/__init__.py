cdea1bee24f9de3225e09f1e7f1049eff2382c60
Michael Liao <askxuefeng@gmail.com>
2013-02-13 22:46:05 +0800

    remove theme

--- themes/childdream/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Michael Liao'
-
-name = 'Child\'s Dream'
-description = 'The child\'s dream theme'
-author = 'Michael Liao'
-copyright = 'Copyright(c) 2012'
