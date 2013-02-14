db141d9dd898589eada67dbfff2dcd3864feff67
Michael Liao <askxuefeng@gmail.com>
2013-02-14 23:15:06 +0800

    update static handler, remove unused imports '

--- static_handler.py
+++ static_handler.py
@@ -11,8 +11,7 @@ It is ONLY used in developement. You should use web server (e.g. nginx) to handl
 
 import os, logging, mimetypes
 
-from transwarp.web import ctx, get, post, route, jsonrpc, Template, seeother, HttpError
-from transwarp import db
+from transwarp.web import ctx, get, HttpError
 
 def _static_file_generator(fpath):
     BLOCK_SIZE = 8192
