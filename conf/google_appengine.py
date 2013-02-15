7c6f951d2dd6e0bb94d1301766bd4a2d651a6c54
Michael Liao <askxuefeng@gmail.com>
2013-02-15 16:13:17 +0800

    remove GAE support

--- conf/google_appengine.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Michael Liao'
-
-# Config for Google AppEngine:
-
-SQL_INSTANCE_NAME = 'itranswarpproj:itranswarp'
-SQL_DATABASE_NAME = 'itranswarp'
