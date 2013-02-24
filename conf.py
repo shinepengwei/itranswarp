696fe7bb568639bf5dc1656f6a11654ec885572b
Michael Liao <askxuefeng@gmail.com>
2013-02-24 16:49:42 +0800

    add conf.py

---
+++ conf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+' configurations '
+
+DB = {
+    'type': 'mysql',
+    'schema': 'itranswarp',
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': 'passw0rd',
+}
+
+CACHE = {
+    'type': 'redis',
+    'host': 'localhost',
+}
