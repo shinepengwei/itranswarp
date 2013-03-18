4f69eb7d257cd65c8235220881a2f82da42cd1c4
Michael Liao <askxuefeng@gmail.com>
2013-03-18 10:43:16 +0800

    remove old theme

--- themes/default/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Michael Liao'
-
-name = 'Default'
-description = 'The default theme'
-author = 'Michael Liao'
-copyright = 'Copyright(c) 2012'
