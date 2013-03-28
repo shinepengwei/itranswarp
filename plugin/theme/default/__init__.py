7fd42e1c8ff69b48fa324698fb2235db002f179b
Michael Liao <askxuefeng@gmail.com>
2013-03-28 15:46:14 +0800

    add theme description and preview image

---
+++ plugin/theme/default/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+name = 'Default'
+description = 'Default theme for iTranswarp.'
+author = 'Michael Liao'
+url = 'http://www.itranswarp.com/'
---
+++ plugin/theme/manifest/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+name = 'Manifest'
+description = 'A clean and streamlined theme that focused on the content and not the distractions.'
+author = 'Jim Barraud'
+url = 'http://themes.jimbarraud.com/manifest/'
