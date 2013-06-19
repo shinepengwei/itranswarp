5fd3cbc9d3f176600685f539b663f4126ed790f6
Michael Liao <askxuefeng@gmail.com>
2013-06-19 21:18:04 +0800

    add manage js

---
+++ static/js/manage.js
@@ -0,0 +1,67 @@
+function refresh() {
+    location.assign('?t=' + new Date().getTime());
+}
+
+function show_error(err, field_name) {
+    $('div.control-group').removeClass('error');
+    if (err) {
+        $('.alert-error').text(err).show();
+        if ($('.alert-error').offset().top < $(window).scrollTop()) {
+            $('html,body').animate({scrollTop: $('.alert-error').offset().top});
+        }
+    }
+    else {
+        $('div.alert-error').text('').hide();
+        return false;
+    }
+    if (field_name) {
+        $('.field-' + field_name).addClass('error');
+    }
+    return false;
+}
+
+function show_loading(show) {
+    if (show) {
+        $('button[type=submit]').attr('disabled', 'disabled');
+        $('button[type=submit] > i').addClass('loading');
+    }
+    else {
+        $('button[type=submit]').removeAttr('disabled');
+        $('button[type=submit] > i').removeClass('loading');
+    }
+}
+
+function show_success() {
+    $('.alert-success').show().delay(3000).slideUp(300);
+    if ($('.alert-success').offset().top < $(window).scrollTop()) {
+        $('html,body').animate({scrollTop: $('.alert-success').offset().top});
+    }
+}
+
+function show_confirm(title, text_or_html, fn_ok, fn_cancel) {
+    var s = '<div class="modal hide fade"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>'
+          + '<h3>' + $('<div/>').text(title).html() + '</h3></div>'
+          + '<div class="modal-body">'
+          + ((text_or_html.length > 0 && text_or_html[0]=='<') ? text_or_html : '<p>' + $('<div/>').text(text_or_html).html() + '</p>')
+          + '</div><div class="modal-footer"><a href="#" class="btn btn-primary"><i class="icon-ok icon-white"></i> OK</a><a href="#" class="btn" data-dismiss="modal"><i class="icon-remove"></i> Cancel</a></div></div>';
+    $('body').prepend(s);
+    var $modal = $('body').children(':first');
+    $modal.modal('show');
+    $modal.find('.btn-primary').click(function() {
+        $modal.attr('result', 'ok');
+        $(this).attr('disabled', 'disabled');
+        $(this).find('i').addClass('loading');
+        fn_ok && fn_ok($(this), function() {
+            $modal.modal('hide');
+        });
+    });
+    $modal.on('hidden', function() {
+        $modal.remove();
+        if ($modal.attr('result')=='ok')
+            fn_cancel && fn_cancel();
+    });
+}
+
+$(function() {
+    $('a[rel=tooltip]').tooltip();
+});
