970544b59b729c8e288286b41591ba152d22ef34
Michael Liao <askxuefeng@gmail.com>
2013-06-19 21:21:39 +0800

    remove theme manifest

--- plugin/theme/manifest/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-name = 'Manifest'
-description = 'A clean and streamlined theme that focused on the content and not the distractions.'
-author = 'Jim Barraud'
-url = 'http://themes.jimbarraud.com/manifest/'
